93a23e8c68c57c92aa8f33f23e817fa14650972f
blg59@cornell.edu <blg59@cornell.edu>
2013-06-11 15:48:03 -0400

    skeeto-flavored overhaul. TODO delete unused things. There are many.

---
+++ .emacs.d/init.el
@@ -0,0 +1,385 @@
+(add-to-list 'load-path "~/.emacs.d")
+
+;;; Turn off the annoying crap immediately
+(column-number-mode 1)
+(delete-selection-mode t)
+(global-linum-mode 1)
+; (menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+; (blink-cursor-mode -1)
+(setq auto-window-vscroll nil)
+(setq backup-inhibited t)
+(setq blink-matching-delay 0.1)
+(setq auto-save-default nil)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message ";; His petard runneth over. \n;; Actually, I think I'd call this a 'threek'.")
+(setq scroll-conservatively 10000)
+(setq-default truncate-lines t)
+(setq-default indent-tabs-mode nil)
+(setq x-select-enable-clipboard t)
+(setq wdired-allow-to-change-permissions t)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq dabbrev-case-distinction nil)
+(setq dabbrev-case-fold-search nil)
+(setq windmove-wrap-around t)
+(setq echo-keystrokes 0.1)
+(setq delete-active-region nil)
+(setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
+(mapatoms (lambda (sym)
+            (if (get sym 'disabled)
+                (put sym 'disabled nil))))
+
+;;; Packages
+
+(require 'tabulated-list)
+(require 'package)
+(require 'package-helper)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(require 'package)
+
+;; Install packages not needing configuration
+;; (with-package (impatient-mode lua-mode memoize rdp))
+
+;; Load local "packages"
+(require 'imgur)
+(require 'misc)
+
+
+;;; Custom global bindings
+(global-set-key (kbd "C-x C-o") 'windmove-down)
+(global-set-key (kbd "C-x C-p") 'windmove-right)
+(global-set-key (kbd "C-S-j") 'join-line)
+(global-set-key "\C-x;" 'goto-line)
+(global-set-key "\C-x\C-k" 'compile)
+;; (global-set-key [f2] (expose #'revert-buffer nil t))
+(global-set-key [f5] (lambda () (interactive) (mapatoms 'byte-compile)))
+(global-set-key "$" 'match-paren)
+(global-set-key (kbd "C-c d") 'insert-date)
+
+
+;;; auto-mode-alist entries
+
+(add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
+(add-to-list 'auto-mode-alist '("\\.mom$" . nroff-mode))
+
+
+;;; Individual package configurations
+
+(require 'tex-site)
+
+(load-file "~/.emacs.d/ProofGeneral-4.2/generic/proof-site.el")
+
+(add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-tm)
+
+(add-to-list 'load-path "~/.emacs.d/tuareg-mode")
+(autoload 'tuareg-mode "tuareg" "Major mode for OCaml" t)
+(add-to-list 'auto-mode-alist '("[.]ml[liy]?$" . tuareg-mode))
+;; (autoload 'tuareg-run-ocaml "tuareg" "Run an inferior OCaml process" t)
+(setq tuareg-in-indent 0)
+
+;; (defalias 'lisp-interaction-mode 'emacs-lisp-mode)
+;; (defun ielm-repl ()
+;;   (interactive)
+;;   (pop-to-buffer (get-buffer-create "*ielm*"))
+;;   (ielm))
+;; (defun ert-silently ()
+;;   (interactive)
+;;   (ert t))
+;; (define-key emacs-lisp-mode-map (kbd "C-x r") (expose #'ert t))
+;; (define-key emacs-lisp-mode-map (kbd "C-c C-z") 'ielm-repl)
+;; (define-key emacs-lisp-mode-map (kbd "C-c C-k") 'eval-buffer)
+;; (font-lock-add-keywords 'emacs-lisp-mode
+;;                         '(("(\\<\\(\\(?:ert-\\)?deftest\\)\\> +\\([^ ()]+\\)"
+;;                            (1 'font-lock-keyword-face)
+;;                            (2 'font-lock-function-name-face))))
+
+(with-package* time
+  (setq display-time-default-load-average nil)
+  (setq display-time-use-mail-icon t)
+  (setq display-time-24hr-format t)
+  (display-time-mode t))
+
+;; I dunno what this does
+(with-package comint
+  (message "comint loaded: %s" (featurep 'comint))
+  (setq comint-prompt-read-only t
+        comint-history-isearch t)
+  (define-key comint-mode-map (kbd "<down>") 'comint-next-input)
+  (define-key comint-mode-map (kbd "<up>") 'comint-previous-input)
+  (define-key comint-mode-map (kbd "C-n") 'comint-next-input)
+  (define-key comint-mode-map (kbd "C-p") 'comint-previous-input)
+  (define-key comint-mode-map (kbd "C-r") 'comint-history-isearch-backward))
+
+(with-package tramp
+  (setq tramp-persistency-file-name
+    (concat temporary-file-directory "tramp-" (user-login-name))))
+
+;; ;; Not working
+;; (with-package* whitespace-cleanup
+;;   (setq-default indent-tabs-mode nil))
+
+(with-package (simple utility)
+  ;; (skeeto) disable so I don't use it by accident
+  (define-key visual-line-mode-map (kbd "M-q") (expose (lambda ()))))
+
+(with-package uniquify
+  (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
+
+(with-package winner
+  (winner-mode 1)
+  (windmove-default-keybindings))
+
+;; 2013-05-24: Not installing magit today
+;; (with-package magit-autoloads
+;;   (setq vc-display-status nil)
+;;   (global-set-key "\C-xg" 'magit-status))
+
+(with-package markdown-mode-autoloads
+  (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("pentadactyl.txt$" . markdown-mode))
+  (setq sentence-end-double-space nil))
+
+(with-package markdown-mode
+  (define-key markdown-mode-map (kbd "<tab>") nil)) ; fix for YASnippet
+
+;; 2013-05-24: Not installing jekyll
+;; (add-to-list 'load-path "~/.emacs.d/jekyll.el")
+;; (setq jekyll-home "~/src/skeeto.github.com/")
+;; (when (file-exists-p jekyll-home)
+;;   (setq httpd-root (concat jekyll-home "_site"))
+;;   (ignore-errors
+;;     (httpd-start)
+;;     (jekyll/start)))
+;; (defservlet robots.txt text/plain ()
+;;   (insert "User-agent: *\nDisallow: /\n"))
+;; (defservlet uptime "text/plain" ()
+;;   (princ (emacs-uptime)))
+
+(with-package js2-mode
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+  (add-hook 'js2-mode-hook (lambda () (setq mode-name "js2")))
+  (setq js2-skip-preprocessor-directives t)
+  (setq-default js2-additional-externs
+                '("$" "unsafeWindow" "localStorage" "jQuery"
+                  "setTimeout" "setInterval" "location" "skewer"
+                  "console" "phantom")))
+
+;; 2013-05-24: Inline web dev
+;; https://github.com/skeeto/skewer-mode
+;; (with-package (skewer-mode utility)
+;;   (define-key skewer-mode-map (kbd "C-c $")
+;;     (expose #'skewer-bower-load "jquery" "1.9.1")))
+
+(with-package clojure-mode
+  (add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode)))
+n
+(with-package nrepl
+  (defadvice nrepl-popup-buffer-display (after nrepl-focus-errors activate)
+    "Focus the error buffer after errors, like Emacs normally does."
+    (select-window (get-buffer-window nrepl-error-buffer)))
+  (defadvice nrepl-eval-last-expression (after nrepl-flash-last activate)
+    (flash-region (save-excursion (backward-sexp) (point)) (point)))
+  (defadvice nrepl-eval-expression-at-point (after nrepl-flash-at activate)
+    (apply #'flash-region (nrepl-region-for-expression-at-point)))
+  ;; Remove ":headless" to work around Leiningen bug
+  (setq nrepl-server-command "lein repl"))
+
+;; (with-package inf-ruby
+;;   (defadvice inf-ruby-output-filter (after ruby-echo (output) activate)
+;;     (macrolet ((r (regex input) `(replace-regexp-in-string ,regex "" ,input)))
+;;       (let ((echo (r "[ \n\r\t]+$" (r inf-ruby-prompt-pattern output))))
+;;         (when (> (length echo) 0)
+;;           (message "%s" echo)))))
+;;   (defadvice ruby-send-last-sexp (after ruby-flash-last activate)
+;;     (flash-region (save-excursion (ruby-backward-sexp) (point)) (point)))
+;;   (defadvice ruby-send-definition (after ruby-flash-defun activate)
+;;     (save-excursion
+;;       (ruby-end-of-defun)
+;;       (let ((end (point)))
+;;         (ruby-beginning-of-defun)
+;;         (flash-region (point) end)))))
+
+(with-package 'ps-print
+  (setq ps-print-header nil))
+
+(with-package glsl-mode-autoloads
+  (add-to-list 'auto-mode-alist '("\\.glsl$" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.vert$" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.frag$" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.fs$" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.vs$" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.cl$" . c-mode))) ; OpenCL
+
+(with-package erc
+  (when (eq 0 (string-match "wello" (user-login-name)))
+    (setq erc-nick "skeeto")))
+
+(with-package 'cc-mode
+  (setcdr (assq 'c-basic-offset (cdr (assoc "k&r" c-style-alist))) 4)
+  (add-to-list 'c-default-style '(c-mode . "k&r")))
+
+(with-package ielm
+  (defadvice ielm-eval-input (after ielm-paredit activate)
+    "Begin each ielm prompt with a paredit pair.."
+    (paredit-open-round)))
+
+;; 2013-05-24: I dont' think I want this. Reconsider if I ever start doing lisp-dev
+;; (with-package paredit-autoloads
+;;   (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+;;   (add-hook 'lisp-mode-hook 'paredit-mode)
+;;   (add-hook 'scheme-mode-hook 'paredit-mode)
+;;   (add-hook 'ielm-mode-hook 'paredit-mode)
+;;   (add-hook 'clojure-mode-hook 'paredit-mode))
+
+;; 2013-05-24: Source of the paren highlights
+(with-package* paren
+  (show-paren-mode))
+
+(with-package* parenface
+  (set-face-foreground 'paren-face "Gray30")
+  (add-hook 'clojure-mode-hook
+            (paren-face-add-support clojure-font-lock-keywords)))
+
+(with-package* bracket-face
+  (add-hook 'scheme-mode-hook (bracket-face scheme-font-lock-keywords-2))
+  (add-hook 'lisp-mode-hook (bracket-face lisp-font-lock-keywords-2))
+  (add-hook 'emacs-lisp-mode-hook (bracket-face lisp-font-lock-keywords-2))
+  (add-hook 'clojure-mode-hook (bracket-face clojure-font-lock-keywords)))
+
+(add-to-list 'load-path "~/.emacs.d/ido-ubiquitous/")
+(require 'ido-ubiquitous)
+(setq ido-enable-flex-matching t)
+(setq ido-show-dot-for-dired t) ; Old habits die hard! (???)
+(setq ido-everywhere t)
+(ido-mode 1)
+(ido-ubiquitous-mode)
+(setq ido-ubiquitous-enable-compatibility nil)
+
+(with-package* smex
+  (smex-initialize)
+  (global-set-key (kbd "M-x") 'smex))
+
+;; ;; (with-package* custom
+;; ;;   (load-theme 'wombat t)
+;; ;;   (add-hook 'after-make-frame-functions
+;; ;;             (lambda (frame)
+;; ;;               (let ((themes custom-enabled-themes))
+;; ;;                 (mapc 'disable-theme themes)
+;; ;;                 (mapc 'enable-theme (reverse themes)))))
+;; ;;   ;; Fix broken faces between Wombat and Magit
+;; ;;   (custom-set-faces
+;; ;;    '(diff-added           ((t :foreground "green")))
+;; ;;    '(diff-removed         ((t :foreground "red")))
+;; ;;    '(highlight            ((t (:background "black"))))
+;; ;;    '(magit-item-highlight ((t :background "black")))))
+
+;; (with-package* javadoc-lookup-autoloads)
+;;   (global-set-key (kbd "C-h j") 'javadoc-lookup)
+
+(add-to-list 'load-path "~/.emacs.d/javadoc-lookup/")
+(require 'javadoc-lookup)
+(global-set-key (kbd "C-h j") 'javadoc-lookup)
+;; ;; (with-package* javadoc-lookup
+;;   (javadoc-add-artifacts
+;;    [org.lwjgl.lwjgl lwjgl "2.8.2"]
+;;    [com.nullprogram native-guide "0.2"]
+;;    [junit junit "4.10"]
+;;    [org.projectlombok lombok "0.10.4"]
+;;    [org.mockito mockito-all "1.9.0"]
+;;    [com.beust jcommander "1.25"]
+;;    [com.google.guava guava "12.0"]
+;;    [org.jbox2d jbox2d-library "2.1.2.2"]
+;;    [org.apache.commons commons-math3 "3.0"]
+;;    [org.pcollections pcollections "2.1.2"]
+;;    [org.xerial sqlite-jdbc "3.7.2"]
+;;    [com.googlecode.lanterna lanterna "2.1.2"]
+;;    [joda-time joda-time "2.1"]
+;;    [org.apache.lucene lucene-core "3.3.0"])
+;; ;; )
+
+(with-package browse-url
+  (when (executable-find "chromium-browser")
+    (setq browse-url-browser-function 'browse-url-chromium)))
+
+(with-package yasnippet
+  (yas-global-mode 1)
+  (yas/load-directory "~/.emacs.d/yasnippet-java")
+  (defun disable-yas ()
+    (yas-minor-mode -1))
+  (add-hook 'emacs-lisp-mode-hook 'disable-yas))
+
+(with-package* lambda-mode
+  (add-hook 'lisp-mode-hook 'lambda-mode)
+  (add-hook 'python-mode-hook 'lambda-mode)
+  (add-hook 'scheme-mode-hook 'lambda-mode))
+
+;; (with-package multiple-cursors-autoloads
+;;   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+;;   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+;;   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
+;; (add-to-list 'load-path "~/.emacs.d/graphiz-dot.el")
+;; (require 'graphiz-dot)
+;; (with-package graphiz-dot
+;; (setq graphviz-dot-indent-width 2)
+;; (setq graphviz-dot-auto-indent-on-semi nil))
+
+(add-to-list 'load-path "~/.emacs.d/uuid-simple.el")
+(require 'uuid-simple)
+(global-set-key "\C-x!" 'uuid-insert)
+(random (make-uuid))
+
+(add-to-list 'load-path "~/.emacs.d/compile-bind.el")
+(require 'compile-bind)
+(compile-bind* (current-global-map)
+               'make ("C-x c" ""
+                      "C-x r" 'run
+                      "C-x C" 'clean))
+
+;; (add-to-list load-path "~/.emacs.d/haskell-mode/")
+;; (load "~/lib/emacs/haskell-mode/haskell-site-file")
+;; (require 'haskell-mode)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-interactive-mode)
+;; ;; Choose  one indentation option
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; ; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;; ; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+(setq load-path (cons "/usr/local/lib/kics2-0.2.4/tools/emacs/" load-path))
+(setq auto-mode-alist
+      (append auto-mode-alist
+              '(("\\.curry$"  . curry-mode)
+                ("\\.lcurry$"  . literate-curry-mode))))
+(autoload 'curry-mode "curry-mode"
+         "Major mode for editing Curry programs." t)
+(autoload 'literate-curry-mode "curry-mode"
+         "Major mode for editing literate Curry scripts." t)
+(add-hook 'curry-mode-hook 'turn-on-curry-font-lock)
+(add-hook 'curry-mode-hook 'turn-on-curry-decl-scan)
+(add-hook 'curry-mode-hook 'turn-on-curry-pakcs)
+
+(with-package coffee-mode
+  (add-to-list 'auto-mode-alist '("[.]cunit$" . coffee-mode))
+  (add-to-list 'auto-mode-alist '("[.]icoffee$" . coffee-mode)))
+
+(require 'jinja2-mode)
+
+(with-package* fic-mode
+  (add-hook 'c-mode-hook 'turn-on-fic-mode)
+  (add-hook 'coffee-mode-hook 'turn-on-fic-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-fic-mode)
+  (add-hook 'java-mode-hook 'turn-on-fic-mode)
+  (add-hook 'latex-mode-hook 'turn-on-fic-mode)
+  (add-hook 'python-mode-hook 'turn-on-fic-mode)
+  (add-hook 'tuareg-mode-hook 'turn-on-fic-mode))
+
+(provide 'init) ; make (require 'init) happy
---
+++ .emacs.d/misc.el
@@ -0,0 +1,197 @@
+;;; misc.el --- a bunch of small clutter that doesn't fit elsewhere
+
+;;; Code:
+
+(defun insert-date (prefix)
+  "Insert the current date. With prefix-argument, use ISO format. With two prefix arguments, write out the day and month name."
+  (interactive "P")
+  (let ((format (cond 
+                 ((not prefix) "%Y-%m-%d")
+                 ((equal prefix '(4)) "%Y-%m-%d")
+                 ((equal prefix '(16)) "%A, %d. %B %Y")))
+        (system-time-locale "C"))
+    (insert (format-time-string format))))
+
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
+;; Move line functions
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    (forward-line -1)
+    (forward-char col)))
+
+(defun move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (move-line (if (null n) -1 (- n))))
+
+(defun move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (move-line (if (null n) 1 n)))
+
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
+
+;; Convert endlines
+(defun dos2unix ()
+  "Convert a buffer from dos ^M end of lines to unix end of lines"
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\r" nil t) (replace-match "")))
+
+(defun unix2dos ()
+  "Opposite of dos2unix"
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\n" nil t) (replace-match "\r\n")))
+
+;; Takes a multi-line paragraph and makes it into a single line of text
+(defun unfill-paragraph ()
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+;; Fibonacci
+
+(defun fib (n)
+  "Fibanacci sequence, slow implementation."
+  (if (<= n 2) 1
+    (+ (fib (- n 2)) (fib (- n 1)))))
+
+;; Window width
+
+(defun set-window-width (n)
+  "Set the selected window's width."
+  (adjust-window-trailing-edge (selected-window) (- n (window-width)) t))
+
+(defun set-80-columns ()
+  "Set the selected window to 80 columns. If given a prefix
+argument, set so that number of columns instead."
+  (interactive)
+  (set-window-width (or current-prefix-arg 80)))
+
+(global-set-key "\C-x~" 'set-80-columns)
+
+;; ID: 6a3f3d99-f0da-329a-c01c-bb6b868f3239
+(defmacro measure-time (&rest body)
+  "Measure and return the running time of the code block."
+  (declare (indent defun))
+  (let ((start (make-symbol "start")))
+    `(let ((,start (float-time)))
+       ,@body
+       (- (float-time) ,start))))
+
+(defun insert-random (n)
+  "Insert a random number between 0 and the prefix argument."
+  (interactive "P")
+  (insert (number-to-string (random n))))
+(global-set-key (kbd "C-c r") 'insert-random)
+
+(defun eval-and-replace (value)
+  "Evaluate the sexp at point and replace it with its value."
+  (interactive (list (eval-last-sexp nil)))
+  (kill-sexp -1)
+  (insert (format "%S" value)))
+
+(global-set-key "\C-ce" 'eval-and-replace)
+
+(defun launch (command)
+  "Launch an application from Emacs, with its own output
+buffer. This is like asynch-shell-command but allows for any
+number of processes at a time, rather than just one. If given a
+prefix argument, the process's buffer is displayed."
+  (interactive (list (read-shell-command (concat default-directory "$ "))))
+  (let* ((name (car (split-string-and-unquote command)))
+         (buffer (generate-new-buffer (concat "*" name "*"))))
+    (set-process-sentinel (start-process-shell-command name buffer command)
+                          'launch-sentinel)
+    (if (eq (car current-prefix-arg) 4)
+        (display-buffer buffer))))
+
+(defun launch-sentinel (proc event)
+  "Reports on changes in `launch'ed applications."
+  (message (format "%s: %s" proc event)))
+
+(global-set-key (kbd "s-x") 'launch)
+
+;; Dictionary lookup
+
+(defun lookup-word (word)
+  (interactive (list (save-excursion (car (ispell-get-word nil)))))
+  (browse-url (format "http://en.wiktionary.org/wiki/%s" word)))
+
+(global-set-key (kbd "M-#") 'lookup-word)
+
+;; Region fashing (from Slime)
+
+(defun flash-region (start end &optional timeout)
+  "Temporarily highlight region from START to END."
+  (let ((overlay (make-overlay start end)))
+    (overlay-put overlay 'face 'secondary-selection)
+    (run-with-timer (or timeout 0.2) nil 'delete-overlay overlay)))
+
+;; File input
+
+(defun slurp (file)
+  "Return FILE contents as a string."
+  (with-temp-buffer
+    (insert-file-contents file)
+    (buffer-string)))
+
+;; Quick switch to scratch buffers
+
+(defmacro scratch-key (key buffer-name mode)
+  `(global-set-key ,key (lambda ()
+                          (interactive)
+                          (switch-to-buffer ,buffer-name)
+                          (unless (eq major-mode ',mode)
+                            (,mode)))))
+
+;; (scratch-key (kbd "C-S-s") "*scratch*"    emacs-lisp-mode)
+;; (scratch-key (kbd "C-S-d") "*javascript*" js2-mode)
+;; (scratch-key (kbd "C-S-a") "*lisp*"       lisp-mode)
+;; (scratch-key (kbd "C-S-c") "*clojure*"    clojure-mode)
+;; (scratch-key (kbd "C-S-x") "*css*"        css-mode)
+;; (scratch-key (kbd "C-S-h") "*html*"       html-mode)
+
+;; ID: 72dc0a9e-c41c-31f8-c8f5-d9db8482de1e
+(defun find-all-files (dir)
+  "Open all files and sub-directories below the given directory."
+  (interactive "DBase directory: ")
+  (let* ((list (directory-files dir t "^[^.]"))
+         (files (remove-if 'file-directory-p list))
+         (dirs (remove-if-not 'file-directory-p list)))
+    (dolist (file files)
+      (find-file-noselect file))
+    (dolist (dir dirs)
+      (find-file-noselect dir)
+      (find-all-files dir))))
+
+;; Dedicated windows
+(defun toggle-current-window-dedication ()
+  (interactive)
+  (let* ((window (selected-window))
+         (dedicated (window-dedicated-p window)))
+    (set-window-dedicated-p window (not dedicated))
+    (message "Window %sdedicated to %s"
+             (if dedicated "no longer " "")
+             (buffer-name))))
+
+(global-set-key [pause] 'toggle-current-window-dedication)
+
+(provide 'misc)
+
+;;; misc.el ends here
